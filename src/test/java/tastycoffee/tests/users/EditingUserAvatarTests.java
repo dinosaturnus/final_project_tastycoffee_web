39c44dbfa6bbcb453914f9c0f295d55847ae6649
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-08-02 01:32:19 +0300

    redaction

--- src/test/java/tastycoffee/tests/users/EditingUserAvatarTests.java
+++ src/test/java/tastycoffee/tests/users/EditingUserAvatarTests.java
@@ -1,12 +1,10 @@
 package tastycoffee.tests.users;
 
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import tastycoffee.pages.users.EditingUserAvatarPage;
@@ -30,6 +28,11 @@ public class EditingUserAvatarTests extends TestBase {
         step("Открыть страницу личного кабинета", () -> {
             editingUserAvatarPage.openAccountPage();
         });
+    }
+
+    @AfterAll
+    static void clearBrowserCookies() {
+        Selenide.clearBrowserCookies();
     }
 
     @CsvFileSource(resources = "/avatars_path.csv")
